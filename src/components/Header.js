7223604894a4d444d3f06c94edd8b32658f62c1b
Guilherme Muller <guilherme.muller1995@gmail.com>
2020-12-20 15:13:02 -0300

    projeto feito com ReactJS

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+export default ({black}) => {
+    return (
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://assets.brand.microsites.netflix.io/assets/493f5bba-81a4-11e9-bf79-066b49664af6_cm_1440w.png?v=46" alt="Netflix" />
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src="https://pbs.twimg.com/profile_images/1267424449021849606/Ge4BiRIe.jpg" alt="Usuário"/>
+                </a>
+            </div>
+        </header>
+    );
+}
